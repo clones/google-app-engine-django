07bb0424110cb7b9ccd30c2cfd9abf41f865ac40
mattbrown.nz <mattbrown.nz@d4307497-c249-0410-99bd-594fbd7e173e>
2008-05-16 02:03:41 +0000

    Move the location where a local copy of the SDK is expected to be found from 'google_appengine' to '.google_appengine'. This means that the upload process will automatically ignore the SDK without having to rely on the user to correctly modify skip_files.
    The old 'google_appengine' location will continue to work, with a deprecation
    warning for the next release or two.
    
    
    git-svn-id: http://google-app-engine-django.googlecode.com/svn/trunk@22 d4307497-c249-0410-99bd-594fbd7e173e

--- appengine_django/__init__.py
+++ appengine_django/__init__.py
@@ -56,19 +56,21 @@ except ImportError, e:
   # Not on the system path. Build a list of alternative paths where it may be.
   # First look within the project for a local copy, then look for where the Mac
   # OS SDK installs it.
-  paths = [PARENT_DIR, '/usr/local']
+  paths = [os.path.join(PARENT_DIR, '.google_appengine'),
+           os.path.join(PARENT_DIR, 'google_appengine'),
+           '/usr/local/google_appengine']
   # Then if on windows, look for where the Windows SDK installed it.
   try:
     import win32api
     ROOT_PATH = os.path.dirname(win32api.GetWindowsDirectory())
-    paths.append(os.path.join(ROOT_PATH, 'Program Files', 'Google'))
+    paths.append(os.path.join(ROOT_PATH, 'Program Files', 'Google',
+                              'google_appengine'))
   except ImportError, e:
     # Not windows.
     pass
   # Loop through all possible paths and look for the SDK dir.
   SDK_PATH = None
-  for base_path in paths:
-    sdk_path = os.path.join(base_path, 'google_appengine')
+  for sdk_path in paths:
     if os.path.exists(sdk_path):
       SDK_PATH = sdk_path
       break
@@ -77,6 +79,12 @@ except ImportError, e:
     sys.stderr.write("The Google App Engine SDK could not be found!\n")
     sys.stderr.write("See README for installation instructions.\n")
     sys.exit(1)
+  if SDK_PATH == os.path.join(PARENT_DIR, 'google_appengine'):
+    logging.warn('Loading the SDK from the \'google_appengine\' subdirectory '
+                 'is now deprecated!')
+    logging.warn('Please move the SDK to a subdirectory named '
+                 '\'.google_appengine\' instead.')
+    logging.warn('See README for further details.')
   # Add the SDK and the libraries within it to the system path.
   EXTRA_PATHS = [
       SDK_PATH,
