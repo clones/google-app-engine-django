b4810235dd91e326b088f0ea4fc479ad6b9b7900
mattbrown.nz <mattbrown.nz@d4307497-c249-0410-99bd-594fbd7e173e>
2008-09-10 16:26:42 +0000

    Improve session backend compatibility with Django.
    Patch submitted by Bernd Dorn of Lovely Systems.
    
    Fixes #85.
    
    
    git-svn-id: http://google-app-engine-django.googlecode.com/svn/trunk@59 d4307497-c249-0410-99bd-594fbd7e173e

--- appengine_django/sessions/backends/db.py
+++ appengine_django/sessions/backends/db.py
@@ -21,9 +21,6 @@ from django.core.exceptions import SuspiciousOperation
 
 from appengine_django.sessions.models import Session
 
-from google.appengine.ext import db
-
-
 class SessionStore(base.SessionBase):
   """A key-based session store for Google App Engine."""
 
@@ -35,14 +32,13 @@ class SessionStore(base.SessionBase):
       except SuspiciousOperation:
         # Create a new session_key for extra security.
         pass
-    self.session_key = self._get_new_session_key()
-    self._session_cache = {}
-    self.save()
-    # Ensure the user is notified via a new cookie.
-    self.modified = True
+    self.create()
     return {}
 
-  def save(self):
+  def save(self, must_create=False):
+    if must_create:
+      if Session.get_by_key_name('k:' + self._session_key):
+        raise base.CreateError
     session = Session(
         key_name='k:' + self.session_key,
         session_data = self.encode(self._session),
@@ -52,7 +48,9 @@ class SessionStore(base.SessionBase):
   def exists(self, session_key):
     return self._get_session(session_key) is not None
 
-  def delete(self, session_key):
+  def delete(self, session_key=None):
+    if session_key is None:
+      session_key = self._session_key
     session = self._get_session(session_key)
     if session:
       session.delete()
@@ -64,3 +62,19 @@ class SessionStore(base.SessionBase):
         return session
       session.delete()
     return None
+
+  def create(self):
+    while True:
+      self.session_key = self._get_new_session_key()
+      try:
+        # Save immediately to ensure we have a unique entry in the
+        # database.
+        self.save(must_create=True)
+      except base.CreateError:
+        # Key wasn't unique. Try again.
+        continue
+      self.modified = True
+      self._session_cache = {}
+      return
+
+
